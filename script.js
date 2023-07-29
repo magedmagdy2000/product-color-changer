c37d400bd31e3c9a39f92e554da8b3c569adb27c
maged magdy <100667701+magedmagdy2000@users.noreply.github.com>
2023-07-29 19:05:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+
+const COLOR_BTNS = document.querySelectorAll('.color');
+COLOR_BTNS.forEach(color => {
+    color.addEventListener('click', () => {
+        let colorNameClass = color.className;
+        if(!color.classList.contains('active-color')){
+            let colorName = colorNameClass.slice(colorNameClass.indexOf('-') + 1, colorNameClass.length);
+            resetActiveBtns();
+            color.classList.add('active-color');
+            setNewColor(colorName)
+        }
+    });
+})
+
+// resetting all color btns
+function resetActiveBtns(){
+    COLOR_BTNS.forEach(color => {
+        color.classList.remove('active-color');
+    });
+}
+
+// set new color on the banner right 
+function setNewColor(color){
+    document.querySelector('.banner-right img').src = `images/tshirt_${color}.png`;
+};
+
+
+
+
+
+$(function () {
+    $(".xzoom, .xzoom-gallery").xzoom({
+      zoomWidth: 400,
+      tint: "#333",
+      Xoffset: 15,
+    });
+  });
+  
